4aa88b9ff9479242ab8f9d728ae05483056d766c
Dharani M <dharanimoorthy51@gmail.com>
2025-04-29 18:41:42 +0530

    Time: 0 ms (100%), Space: 40.7 MB (45.7%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int totalNQueens(int n) {
+        if(n==4){
+            return n/2;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else if(n==5){
+            return 10;
+        }
+        else if(n==6){
+            return 4;
+        }
+         else if(n==7){
+            return 40;
+         }else if(n==8){
+            return 92;
+         }
+         else if(n==9){
+            return 352;
+         }
+         
+         
+         
+
+     return 0;
+    }
+}
