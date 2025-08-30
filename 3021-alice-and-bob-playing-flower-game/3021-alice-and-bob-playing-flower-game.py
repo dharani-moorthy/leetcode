4dbf2572aa05c45de14b080b6eed3a4c6da54b69
Dharani M <dharanimoorthy51@gmail.com>
2025-08-30 21:44:32 +0530

    Time: 2 ms (18.75%), Space: 12.6 MB (18.75%) - LeetHub

---
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def flowerGame(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        a=n*m/2
+        return a
+        
