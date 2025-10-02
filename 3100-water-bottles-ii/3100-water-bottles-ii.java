75b8c45ab0bb9e3d808e976287286b55680aac6b
Dharani M <dharanimoorthy51@gmail.com>
2025-10-02 11:31:01 +0530

    Time: 0 ms (100%), Space: 40.8 MB (73.55%) - LeetHub

---
+++ 3100-water-bottles-ii/3100-water-bottles-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int total=numBottles;
+        int empty=numBottles;
+        while(empty>=numExchange){
+           empty-=numExchange;
+           total+=1;
+           empty+=1;
+           numExchange+=1;
+        }
+        return total;
+    }
+}
